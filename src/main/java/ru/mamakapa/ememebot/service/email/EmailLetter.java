dc1222e736f70cbfea7eb43c5c1e6b185b53b78e
Nikita Kuritsyn <nyan18032003@gmail.com>
2022-11-08 18:46:56 +0300

    create class EmailLetter

--- src/main/java/ru/mamakapa/ememebot/service/email/EmailLetter.java
+++ src/main/java/ru/mamakapa/ememebot/service/email/EmailLetter.java
@@ -1,4 +1,16 @@
 package ru.mamakapa.ememebot.service.email;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
 public class EmailLetter {
+    private String Envelope;
+    private String bodyPart;
+    private List<String> attachmentFilePaths;
+    private List<String> htmlFilePaths;
+    private List<String> links;
 }
