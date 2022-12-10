dbc5bd0aa44ce8a7210ee72da80fe2c0b16decfc
Nikita Kuritsyn <nyan18032003@gmail.com>
2022-12-10 21:11:21 +0300

    create runnable function + add temlates directory

--- src/main/java/ru/mamakapa/ememebot/Operator.java
+++ src/main/java/ru/mamakapa/ememebot/Operator.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import ru.mamakapa.ememebot.config.ImapConfig;
 import ru.mamakapa.ememebot.service.email.EmailLetter;
@@ -27,28 +28,32 @@ public class Operator {
     private static final int TIME_SLEEP = 3000;
     @Value("${vk.user.group_id}")
     private static int USER_GROUP_ID;
+    @Scheduled(fixedRate = TIME_SLEEP)
     public void run(){
         try {
             log.info("I'm listening...");
-            emailService.getEmailConnection().connectToEmail(imapConfig);
+            if (!imapConfig.isConnected()) emailService.getEmailConnection().connectToEmail(imapConfig);
             List<Message> messages;
-            while (true){
+           // while (true){
                 try {
                     messages = emailService.getEmailConnection().getLastMessages(imapConfig,
                             emailService.getEmailConnection().checkUpdates(imapConfig));
                     if (messages != null) {
                         for (Message message : messages) {
                             EmailLetter letter = emailService.getEmailCompiler().constructLetter(message);
-                            sender.sendMessage(letter, USER_GROUP_ID);
-                            emailService.deleteLetter(letter);
-
+                            try {
+                                sender.sendMessage(letter, USER_GROUP_ID);
+                            }catch (Exception e){
+                                System.out.println(e.getMessage());
+                            } finally {
+                                emailService.deleteLetter(letter);
+                            }
                         }
                     }
-                    Thread.sleep(TIME_SLEEP);
                 }catch (Exception e){
                     System.out.println(e.getMessage());
                 }
-            }
+           // }
         } catch (Exception e) {
             System.out.println("Error connect! With exception: " + e.getMessage());
         }
