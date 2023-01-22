25fb554a0f285711618e858c591714b5673fa56b
Nikita Kuritsyn <nyan18032003@gmail.com>
2023-01-22 09:56:16 +0300

    File seps in Saving Dir

--- src/main/java/ru/mamakapa/ememebot/service/email/AbstractEmailCompiler.java
+++ src/main/java/ru/mamakapa/ememebot/service/email/AbstractEmailCompiler.java
@@ -5,12 +5,14 @@ import lombok.Setter;
 
 import javax.mail.Message;
 import javax.mail.Part;
+import java.io.File;
 import java.io.IOException;
 
 @Getter
 @Setter
 public abstract class AbstractEmailCompiler implements EmailCompiler{
-    protected final static String SAVING_DIRECTORY = System.getProperty("user.dir") + "\\src\\main\\resources\\";
+    protected final static String SAVING_DIRECTORY = System.getProperty("user.dir") +
+            File.separator + "src" + File.separator + "main" + File.separator + "resources" + File.separator;
 
     protected abstract String processPart(Part p) throws Exception;
     protected abstract String processEnvelope(Message message) throws Exception;
