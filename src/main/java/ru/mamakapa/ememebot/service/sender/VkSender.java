4c6f300ab29c6fedb0768c49c9702921f37e038e
maksmolchdmitr <62752481+maksmolchdmitr@users.noreply.github.com>
2023-02-13 14:43:13 +0300

    change uploading documents

--- src/main/java/ru/mamakapa/ememebot/service/sender/VkSender.java
+++ src/main/java/ru/mamakapa/ememebot/service/sender/VkSender.java
@@ -12,8 +12,6 @@ import com.vk.api.sdk.queries.upload.UploadPhotoMessageQuery;
 import lombok.extern.slf4j.Slf4j;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import ru.mamakapa.ememebot.service.email.EmailLetter;
 import ru.mamakapa.ememebot.service.sender.exceptions.SendMessageException;
 
@@ -21,31 +19,18 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URI;
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Random;
-import java.util.Set;
 
 @Slf4j
 public class VkSender extends AbstractSender {
-    private TransportClient transportClient;
-    private VkApiClient vk;
-    private GroupActor actor;
-    private Random random = new Random();
+    private final VkApiClient vk;
+    private final GroupActor actor;
+    private final Random random = new Random();
     public static final int GROUP_PEER_ID = 2000000000;
     private static final int MAX_MESSAGE_LENGTH = 4096;
-    private static final Set<String> fileExtensionsDenied = new HashSet<>(Arrays.asList(
-            "exe",
-            "mp3"
-    ));
-    private static final Set<String> photoExtensions = new HashSet<>(Arrays.asList(
-            "jpg",
-            "png",
-            "gif"));
     public VkSender(VkBotConfig vkBotConfig){
         this.setBotConfig(vkBotConfig);
-        this.transportClient = new HttpTransportClient();
-        this.vk = new VkApiClient(this.transportClient);
+        this.vk = new VkApiClient(new HttpTransportClient());
         this.actor = new GroupActor(Integer.parseInt(vkBotConfig.getIdentificator()),
                 vkBotConfig.getToken());
     }
@@ -86,10 +71,10 @@ public class VkSender extends AbstractSender {
         }
     }
 
-    private void sendMessageAttachments(EmailLetter emailLetter, int recipientId) throws IOException, ClientException, ApiException {
+    private void sendMessageAttachments(EmailLetter emailLetter, int recipientId) throws IOException, ClientException, ApiException, SendMessageException {
         if(emailLetter.getHtmlFilePaths().size() !=0 || emailLetter.getAttachmentFilePaths().size() != 0){
             MessagesSendQuery message = this.getMessagesSendQuery(recipientId);
-            StringBuilder attachmentStringBuilder = new StringBuilder("");
+            StringBuilder attachmentStringBuilder = new StringBuilder();
             for (String path : emailLetter.getHtmlFilePaths())
                 attachmentStringBuilder.append(getUploadPhotoAttachId(new File(path), recipientId)).append(",");
             for (String path : emailLetter.getAttachmentFilePaths())
@@ -108,30 +93,33 @@ public class VkSender extends AbstractSender {
             return this.vk.messages().send(this.actor).userId(peerId).randomId(random.nextInt(10000));
         }
     }
-    public String getUploadDocAttachId(File file, int peerId) throws IOException, ClientException, ApiException, JSONException {
+    public String getUploadDocAttachId(File file, int peerId) throws IOException, ClientException, ApiException, JSONException, SendMessageException {
         log.info("Uploading document in attachments");
-        String[] fileParts = file.getName().split("\\.");
-        String fileExtension = fileParts[fileParts.length-1];
-        if(fileExtensionsDenied.contains(fileExtension)){
-            File tempFile = new File(file.getAbsolutePath()+".delete.me");
-            if(!file.renameTo(tempFile)){
-                throw new FileNotFoundException("Not successful renaming file...");
-            }
-            file = tempFile;
-        }
         UploadDocQuery uploadDocQuery = vk.upload().doc(vk.docs().getMessagesUploadServer(actor).peerId(peerId).execute().
                 getUploadUrl().toString(), file);
         JSONObject jsonObject = new JSONObject(uploadDocQuery.executeAsString());
+        if(jsonObject.has("error")){
+            file = renameFile(file);
+            uploadDocQuery = vk.upload().doc(vk.docs().getMessagesUploadServer(actor).peerId(peerId).execute().
+                    getUploadUrl().toString(), file);
+            jsonObject = new JSONObject(uploadDocQuery.executeAsString());
+        }
         try{
             JSONObject json = new JSONObject(this.vk.docs().save(this.actor, jsonObject.getString("file")).executeAsString());
             JSONObject jsonObj = json.getJSONObject("response").getJSONObject("doc");
             return "doc"+jsonObj.getLong("owner_id")+"_"+jsonObj.getLong("id");
         }catch (JSONException e){
-            System.out.println("Add extension '"+fileExtension+"' in DENIED EXTENSIONS!!!");
-            return "";
+            throw new SendMessageException();
         }
     }
-    public String getUploadPhotoAttachId(File file, int peerId) throws IOException, ClientException, ApiException, JSONException {
+    private File renameFile(File file) throws FileNotFoundException {
+        File tempFile = new File(file.getAbsolutePath()+".delete.me");
+        if(!file.renameTo(tempFile)){
+            throw new FileNotFoundException("Not successful renaming file...");
+        }
+        return tempFile;
+    }
+    public String getUploadPhotoAttachId(File file, int peerId) throws ClientException, ApiException, JSONException {
         log.info("Uploading photo in attachments");
         URI url = vk.photos().getMessagesUploadServer(actor).peerId(peerId).execute().getUploadUrl();
         UploadPhotoMessageQuery uploadPhotoMessageQuery = vk.upload().photoMessage(url.toString(), file);
