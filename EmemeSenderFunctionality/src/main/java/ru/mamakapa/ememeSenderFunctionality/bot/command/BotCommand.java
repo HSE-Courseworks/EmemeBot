7a5343cf406610d7c8c10547086d8067b85c1048
maksmolchdmitr <maksmolchdmitr@gmail.com>
2023-04-23 11:16:23 +0300

    inject message in abstract command execution

--- EmemeSenderFunctionality/src/main/java/ru/mamakapa/ememeSenderFunctionality/bot/command/BotCommand.java
+++ EmemeSenderFunctionality/src/main/java/ru/mamakapa/ememeSenderFunctionality/bot/command/BotCommand.java
@@ -1,11 +1,11 @@
 package ru.mamakapa.ememeSenderFunctionality.bot.command;
 
-public abstract class BotCommand {
+public abstract class BotCommand<MESSAGE> {
     protected final String command;
 
     protected BotCommand(String command) {
         this.command = command;
     }
 
-    public abstract void execute();
+    public abstract void execute(MESSAGE message);
 }
--- EmemeSenderFunctionality/src/main/java/ru/mamakapa/ememeSenderFunctionality/bot/command/CommandHandler.java
+++ EmemeSenderFunctionality/src/main/java/ru/mamakapa/ememeSenderFunctionality/bot/command/CommandHandler.java
@@ -3,20 +3,23 @@ package ru.mamakapa.ememeSenderFunctionality.bot.command;
 import ru.mamakapa.ememeSenderFunctionality.bot.command.exception.NonHandleCommandException;
 
 import java.util.List;
+import java.util.function.Function;
 
-public final class CommandHandler {
-    private final List<BotCommand> commands;
+public final class CommandHandler<MESSAGE> {
+    private final List<BotCommand<MESSAGE>> commands;
+    private final Function<MESSAGE, String> getCommandNameFunction;
 
-    public CommandHandler(List<BotCommand> commands) {
+    public CommandHandler(List<BotCommand<MESSAGE>> commands, Function<MESSAGE, String> commandNameFunction) {
         this.commands = commands;
+        this.getCommandNameFunction = commandNameFunction;
     }
 
-    public void handle(String command){
+    public void handle(MESSAGE message){
         commands.stream()
-                .filter(abstractBotCommand -> abstractBotCommand.command.equals(command))
+                .filter(abstractBotCommand -> abstractBotCommand.command.equals(getCommandNameFunction.apply(message)))
                 .findFirst()
-                .orElseThrow(() -> new NonHandleCommandException(command))
-                .execute();
+                .orElseThrow(() -> new NonHandleCommandException(getCommandNameFunction.apply(message)))
+                .execute(message);
     }
 
 
