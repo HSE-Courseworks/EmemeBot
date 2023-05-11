2ea8e0d99d2052fb72f7dd2631a4afa1947ea60e
maksmolchdmitr <maksmolchdmitr@gmail.com>
2023-05-11 15:34:49 +0300

    fix config props

--- TelegramBot/src/main/java/ru/mamakapa/telegramBot/TelegramBotApplication.java
+++ TelegramBot/src/main/java/ru/mamakapa/telegramBot/TelegramBotApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import ru.mamakapa.telegramBot.configuration.KafkaConfig;
+import ru.mamakapa.telegramBot.configuration.ServiceConfiguration;
 import ru.mamakapa.telegramBot.configuration.TelegramConfiguration;
 
 @SpringBootApplication
-@EnableConfigurationProperties({TelegramConfiguration.class, KafkaConfig.class})
+@EnableConfigurationProperties({TelegramConfiguration.class, KafkaConfig.class, ServiceConfiguration.class})
 public class TelegramBotApplication {
     public static void main(String[] args) {
         SpringApplication.run(TelegramBotApplication.class, args);
