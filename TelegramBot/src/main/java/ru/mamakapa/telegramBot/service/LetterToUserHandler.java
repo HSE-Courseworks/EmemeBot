6182bcc46d6effff7a39117f57f9fc9ea98a15be
Молчанов Максим <62752481+maksmolchdmitr@users.noreply.github.com>
2023-05-13 14:11:01 +0300

    S3 Telegram bot (#62)
    * change AddEmailCommand text
    
    * change Email http client
    
    * change Email http client configuration
    
    * fix bug with unhandled message
    
    * change PLACEHOLDER add new email command
    
    * add AWS S3 service
    
    * change LetterToUserHandler with sending files from AWS S3 storage
    
    * fix code style

--- TelegramBot/src/main/java/ru/mamakapa/telegramBot/service/LetterToUserHandler.java
+++ TelegramBot/src/main/java/ru/mamakapa/telegramBot/service/LetterToUserHandler.java
@@ -1,18 +1,50 @@
 package ru.mamakapa.telegramBot.service;
 
 import org.springframework.stereotype.Service;
+import ru.mamakapa.ememeSenderFunctionality.bot.service.FileSender;
 import ru.mamakapa.ememeSenderFunctionality.bot.service.MessageSender;
+import ru.mamakapa.telegramBot.configuration.TelegramConfiguration;
 import ru.mamakapa.telegramBot.data.LetterToUser;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 @Service
 public class LetterToUserHandler {
     private final MessageSender<Integer, String> messageSender;
+    private final AWSService awsService;
+    private final FileSender fileSender;
+    private final static Path FILES_DIRECTORY_PATH = Paths
+            .get("EmemeEmail/src/main/resources/savedir")
+            .toAbsolutePath();
 
-    public LetterToUserHandler(MessageSender<Integer, String> messageSender) {
+    public LetterToUserHandler(
+            MessageSender<Integer, String> messageSender,
+            AWSService awsService,
+            TelegramConfiguration telegramConfiguration
+    ) {
         this.messageSender = messageSender;
+        this.awsService = awsService;
+        this.fileSender = new TelegramFileSender(telegramConfiguration.token());
     }
 
     public void handle(LetterToUser letterToUser) throws Exception {
-        messageSender.send(Math.toIntExact(letterToUser.chatId()), letterToUser.messageContent());
+        int chatId = Math.toIntExact(letterToUser.chatId());
+        List<File> files = letterToUser
+                .fileKeys()
+                .stream()
+                .map(fileKey -> {
+                    try {
+                        return awsService.downloadFile(fileKey, FILES_DIRECTORY_PATH);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .toList();
+        messageSender.send(chatId, letterToUser.messageContent());
+        files.forEach(file -> fileSender.send(chatId, file));
     }
 }
