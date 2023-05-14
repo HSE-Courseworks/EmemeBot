6bb13a442188b75f9e28c4ce184ff4a8c217922d
maksmolchdmitr <maksmolchdmitr@gmail.com>
2023-05-14 06:57:04 +0300

    fix kafka config and consumer

--- VkBot/src/main/java/ru/mamakapa/vkbot/kafka/KafkaMessageConsumer.java
+++ VkBot/src/main/java/ru/mamakapa/vkbot/kafka/KafkaMessageConsumer.java
@@ -1,11 +1,13 @@
 package ru.mamakapa.vkbot.kafka;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.KafkaListener;
 import ru.mamakapa.vkbot.data.LetterToUser;
 import ru.mamakapa.vkbot.service.LetterToUserHandler;
 
 @EnableKafka
+@Slf4j
 public class KafkaMessageConsumer {
     private final LetterToUserHandler letterToUserHandler;
 
@@ -14,7 +16,11 @@ public class KafkaMessageConsumer {
     }
 
     @KafkaListener(topics = "${kafka.topicName}")
-    public void listen(LetterToUser letterToUser) throws Exception {
-        letterToUserHandler.handle(letterToUser);
+    public void listen(LetterToUser letterToUser) {
+        try {
+            letterToUserHandler.handle(letterToUser);
+        } catch (Exception e) {
+            log.error("Handle letterToUser: {} with error: {}", letterToUser.toString(), e.getMessage());
+        }
     }
 }
